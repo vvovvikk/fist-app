93fe2e89d270de8708972ac489fa4cb38ba083d1
vvovvikk <coin@mok.by>
2023-03-02 13:30:28 +0300

    commit

---
+++ src/HelloWorldq.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function HelloWorld() {
+    return (
+      <div>
+        <h1>Hello World!!</h1>
+      </div>
+    );
+  }
+  
+  export default HelloWorld;
