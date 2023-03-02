da87204af270a03d184bd452300bb71ba26d5b37
vvovvikk <coin@mok.by>
2023-03-02 13:37:35 +0300

    commit2

---
+++ src/Menu.js
@@ -0,0 +1,14 @@
+
+
+function Menu() {
+    return (
+      <div>
+        <h1>EEE</h1>
+        <a href="#">Link to google</a>
+        <a href="#">Link to yandex</a>
+        <p>Гиппопотам</p>
+      </div>
+    );
+}
+
+export default Menu;
